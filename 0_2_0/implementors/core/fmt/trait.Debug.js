d433cec954bdf952eebc3ada9a187b90d1b48391
allenbenz <allenbenz@users.noreply.github.com>
2018-01-07 22:10:52 -0800

    added version 0.2.0 docs

---
+++ 0_2_0/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["winrt_notification"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"winrt_notification/enum.Sound.html\" title=\"enum winrt_notification::Sound\">Sound</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"winrt_notification/enum.LoopableSound.html\" title=\"enum winrt_notification::LoopableSound\">LoopableSound</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
