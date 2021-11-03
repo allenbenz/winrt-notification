c25d23f7049eae8740b4c8ced7223efe3eb6911e
allenbenz <allenbenz@users.noreply.github.com>
2021-11-03 13:38:21 -0700

    Add 0.5.0 documentation

---
+++ 0_5_0/crates.js
@@ -0,0 +1,1 @@
+window.ALL_CRATES = ["winrt_notification"];
---
+++ 0_5_0/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["winrt_notification"] = {"name":"","files":["lib.rs","windows_check.rs"]};
+createSourceSidebar();
